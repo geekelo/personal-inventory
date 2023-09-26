68bc7bdf23166449c990946fb495931030643fc2
Ayomide <ayomidesunday31@gmail.com>
2023-09-26 11:16:09 +0100

    Implement methods

--- class/item.rb
+++ class/item.rb
@@ -3,7 +3,7 @@ require 'date'
 class item
   attr_accessor :publish_date, :archived
   attr_reader :genre, :author, :label
-  
+
   def initialize(publish_date, archived: false)
     @id = rand(1_000_000)
     @publish_date = publish_date
@@ -22,11 +22,18 @@ class item
     @label=label
   end
 
-  
+
   def can_be_archived?
     current_date = Date.today
     year = current_date.year - @publish_date.year
     year > 10
+  end
+
+  def move_to_archive
+      if can_be_archived?
+        @archived = true
+      end
+      return @archived
   end
 
 end
