1d783ac52d1709cfab67300b81aa37ea0b8edc6a
geekelo <efuelight12@gmail.com>
2023-09-27 23:02:10 +0100

    Add title attributes to game and book classes

--- class/book.rb
+++ class/book.rb
@@ -2,8 +2,8 @@ class Book < Item
   attr_accessor :author, :publisher, :cover_state, :publish_date, :label
   attr_reader :id
 
-  def initialize(_author, publisher, cover_state, publish_date, label)
-    super(publish_date)
+  def initialize(title, author, publisher, cover_state, publish_date, label)
+    super(title, publish_date)
     @id = rand(100_000)
     @cover_state = cover_state
     @publisher = publisher
--- class/game.rb
+++ class/game.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class Game < Item
   attr_accessor :publish_date, :multiplayer, :last_played_at
 
-  def initialize(publish_date, multiplayer, last_played_at)
-    super(publish_date)
+  def initialize(title, publish_date, multiplayer, last_played_at)
+    super(title, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
