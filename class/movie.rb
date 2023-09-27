ed8188aa507b2f300dfc9d6aed493a2e0e9444d1
geekelo <efuelight12@gmail.com>
2023-09-27 23:47:43 +0100

    Fix linter errors

--- class/movie.rb
+++ class/movie.rb
@@ -1,11 +1,12 @@
 class Movie < Item
   attr_accessor :silent, :title
+
   def initialize(title, publish_date, silent: false)
     super(title, publish_date)
     @silent = silent
   end
 
   def can_be_archived?
-    return true if super || @silent
+    true if super || @silent
   end
 end
