01dd487a98f2cbeb41ee6c9480c113811fba749b
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-27 21:14:21 +0530

    Fix Linters issue

--- class/author.rb
+++ class/author.rb
@@ -1,11 +1,9 @@
-require 'securerandom'
-
 class Author
   attr_accessor :first_name, :last_name, :items
   attr_reader :id
 
   def initialize(first_name, last_name)
-    @id = SecureRandom.uuid
+    @id = rand(1_000_000)
     @first_name = first_name
     @last_name = last_name
     @items = []
