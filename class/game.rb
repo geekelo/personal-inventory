deaa64440e82435fefee1ccf88a44d61b4770319
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-28 15:16:47 +0530

    Add method for class/game.rb

--- class/game.rb
+++ class/game.rb
@@ -1,11 +1,17 @@
 require_relative 'item'
+require 'date'
 
 class Game < Item
   attr_accessor :publish_date, :multiplayer, :last_played_at
 
-  def initialize(title, publish_date, multiplayer, last_played_at)
-    super(title, publish_date)
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    difference = Time.now.year - Date.parse(@last_played_at).year
+    super && difference > 2
   end
 end
