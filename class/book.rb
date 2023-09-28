84d7851f2a917407b34b9fa0698494160092b34d
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-09-28 13:36:20 +0300

    Fixing linter error

--- class/book.rb
+++ class/book.rb
@@ -4,18 +4,18 @@ class Book < Item
   attr_accessor :author, :publisher, :cover_state, :publish_date
   attr_reader :id, :label
 
-  def initialize(author, publisher, cover_state, publish_date)
+  def initialize(_author, publisher, cover_state, publish_date)
     super(publish_date)
-    @id = rand(100000);
+    @id = rand(100_000)
     @cover_state = cover_state
     @publisher = publisher
   end
 
   def can_be_archived?
     if @cover_state == 'bad' || super
-      true        
+      true
     else
       false
-    end 
+    end
   end
 end
