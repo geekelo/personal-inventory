46214a723d24f7a3cac2ec0891fc4c7c37dac475
geekelo <efuelight12@gmail.com>
2023-09-28 10:03:29 +0100

    Fix unit tests errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,13 +7,13 @@ describe Genre do
 
   describe '#add_item' do
     it 'adds an item to the genre' do
-      item = Item.new('Sample Title', '2010-01-01')
+      item = Item.new('2010-01-01')
       genre.add_item(item)
       expect(genre.items).to include(item)
     end
 
     it 'sets the genre of the item' do
-      item = Item.new('Sample Title', '2010-01-01')
+      item = Item.new('2010-01-01')
       genre.add_item(item)
       expect(item.genre).to eq(genre)
     end
--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -7,13 +7,13 @@ describe Source do
 
   describe '#add_item' do
     it 'adds an item to the source' do
-      item = Item.new('Sample Title', '2010-01-01')
+      item = Item.new('2010-01-01')
       source.add_item(item)
       expect(source.items).to include(item)
     end
 
     it 'sets the source of the item' do
-      item = Item.new('Sample Title', '2010-01-01')
+      item = Item.new('2010-01-01')
       source.add_item(item)
       expect(item.source).to eq(source)
     end
