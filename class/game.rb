0e6f8b22399d0e175bccb63f32b41569e55b674b
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-29 15:18:01 +0530

    Modify class/game.rb file

--- class/game.rb
+++ class/game.rb
@@ -2,10 +2,11 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-  attr_accessor :publish_date, :multiplayer, :last_played_at
+  attr_accessor :publish_date, :title, :multiplayer, :last_played_at
 
-  def initialize(publish_date, multiplayer, last_played_at)
+  def initialize(publish_date, title, multiplayer, last_played_at)
     super(publish_date)
+    @title = title
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
